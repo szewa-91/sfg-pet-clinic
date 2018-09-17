97e33c53f0c153edf11204ccd7bbe93d2fd3c4fc
Szewa <marcin91.szewczyk@gmail.com>
2018-09-17 07:48:46 +0200

    Implement BaseEntity

--- pet-clinic-data/src/main/java/eu/marcinszewczyk/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/eu/marcinszewczyk/sfgpetclinic/model/PetType.java
@@ -1,4 +1,13 @@
 package eu.marcinszewczyk.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
